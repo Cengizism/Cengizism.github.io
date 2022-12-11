431ff03d054ae23aa791a35f1f934ec1f0fd0203
cengizism <cengiz.ulusoy@me.com>
2022-12-11 15:52:16 +0100

    Teesting workflow

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : 'Personal portfolio';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'All rights reserved. Cengizism';
 
   return {
     name,
