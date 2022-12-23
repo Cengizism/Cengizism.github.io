7d770f47a31a9e3cf85509f4644de832c3acac38
cengizism <cengiz.ulusoy@me.com>
2022-12-23 06:39:22 +0100

    Add page views to analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Script
