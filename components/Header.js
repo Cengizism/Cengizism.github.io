0d4cec3ccf568ce4af08d7e968da5122bf94b837
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-06-15 10:12:12 +0200

    Add profile image

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,20 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
-          {name}
+          <Image
+            className="block mx-auto w-24 h-24 rounded-full mb-2 bg-gradient-conic from-gradient-3 to-gradient-4"
+            src="/images/cengiz-ulusoy.jpg"
+            alt="test"
+            width={98}
+            height={96}
+          />
         </Link>
+        <Link href="/">{name}</Link>
       </p>
     </header>
   );
