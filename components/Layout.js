3c19ddc4c89be0314530af84da39769614d12f55
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-06-15 10:19:43 +0200

    Extract theme switcher to own component and re-position it to the right of the header

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import { useEffect } from 'react';
+import ThemeSwitcher from './ThemeSwitcher';
 import styles from './Layout.module.css';
 
 export function GradientBackground({ variant, className }) {
@@ -50,10 +51,13 @@ export default function Layout({ children }) {
   }, []);
 
   return (
-    <div className="relative pb-24 overflow-hidden">
-      <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
-        {children}
+    <>
+      <ThemeSwitcher />
+      <div className="relative pb-24 overflow-hidden">
+        <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
