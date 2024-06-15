de760af1be7f3aa4540264c0c698095c36903c08
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-06-15 09:34:47 +0200

    Initialize Google Analytics back again

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,45 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      gtag.pageview(router.pathname);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <span className="theme-bejamas" />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-KL4QWK9J60"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KL4QWK9J60', {
+                page_path: window.location.pathname,
+              });
+            `,
+        }}
+      />
+      
       <Component {...pageProps} />
     </>
   );
